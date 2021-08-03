d0bd1de2f7a915fdf418b84e6f038dab08f6222e
RosiePlatten <r.v.platten@gmail.com>
2021-08-03 16:24:46 +0100

    Added computerPlay function, this function is working as required

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function computerPlay(){
-    return "paper";
+    num = Math.floor(Math.random()*3);
+    switch(num){
+        case 0:
+            return "Rock";
+            break;
+        case 1:
+            return "Paper";
+            break;
+        case 2:
+            return "Scissors";
+            break;
+        default:
+            return "ERROR";
+    }
 }
 
-console.log(computerPlay);
+console.log(computerPlay());
